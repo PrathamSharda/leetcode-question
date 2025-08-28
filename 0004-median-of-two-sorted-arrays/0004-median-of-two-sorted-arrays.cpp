6b2dd228e7f936b6d9ae04bb8802d73d43a1b557
PRATHAM SHARDA <113026312+PrathamSharda@users.noreply.github.com>
2025-08-28 16:58:12 +0530

    0004-MEDIAN-OF-TWO-SORTED-ARRAYS Stats: Time: 0 ms (100%), Space: 95.1 MB (79.69%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int p1=0;
-        int p2=0;
-        vector<int>ans;
-        if(nums1.size()==1&&nums2.size()==0)
-        {
-            return (double) nums1[0];
-        }
-        if(nums2.size()==1&&nums1.size()==0)
-        {
-            return (double) nums2[0];
-        }
 
-        while(p1<nums1.size()&&p2<nums2.size())
-        {
-            if(nums1[p1]<nums2[p2])
-            {
-                ans.push_back(nums1[p1]);
-                p1++;
-            }else{
-                ans.push_back(nums2[p2]);
-                p2++;
-            }
-        }
-
-        while(p1<nums1.size())
-        {
-            ans.push_back(nums1[p1]);
-            p1++;
-        }
-        while(p2<nums2.size())
-        {
-            ans.push_back(nums2[p2]);
-            p2++;
-        }
-        double avg;
-        if(ans.size()%2==0)
-        {
-            p1=ans.size()/2;
-            p2=ans.size()/2-1;
-            avg=double (ans[p1]+ans[p2])/2;
-        }else{
-             p1=ans.size()/2;
-             avg=ans[p1];
-        }
-        return avg;
-
+    // double median(vector<int>&a,vector<int>&b)
+    // {
         
+    // }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+         if(nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+        int l=0;
+        int r=nums1.size();
+
+       int total=nums1.size()+nums2.size();
+       int n1=nums1.size();
+       int n2=nums2.size();
+       if(n1==1&&n2==0)
+       {
+        return nums1[0];
+       }
+       if(n1==0&&n2==1)
+       {
+        return nums2[0];
+       }
+       while(l<=r)
+       {
+            int cut1 = (l + r) / 2;
+            int cut2 = (total + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0 ? INT_MIN : nums1[cut1 - 1]);
+            int r1 = (cut1 == n1 ? INT_MAX : nums1[cut1]);
+            int l2 = (cut2 == 0 ? INT_MIN : nums2[cut2 - 1]);
+            int r2 = (cut2 == n2 ? INT_MAX : nums2[cut2]);
+            
+           if(l1 <= r2 && l2 <= r1) {
+                if(total % 2 == 0)
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+                else
+                    return max(l1, l2);
+            }
+            else if(l1 > r2) {
+                r = cut1 - 1;
+            } else {
+                l = cut1 + 1;
+            }
+
+       }
+      
+       return 0;
     }
 };
