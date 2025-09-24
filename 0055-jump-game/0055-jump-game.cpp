caf8fe8846fc26ed39fc29cb849da49b429b3d16
PRATHAM SHARDA <113026312+PrathamSharda@users.noreply.github.com>
2025-09-24 23:47:02 +0530

    0055-JUMP-GAME Stats: Time: 0 ms (100%), Space: 52.4 MB (35.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        priority_queue<pair<int,int>>pq;
-        unordered_set<int>vis;
-        pq.push({nums[0],0});
-        int index=0;
-        int lastIndex=-1;
-        while(index!=nums.size()-1)
+        int maxReach=0;
+
+        for(int i=0;i<nums.size();i++)
         {
-            if(pq.empty())return false;
-            auto it=pq.top();
-            
-            index=pq.top().second;
-            
-            pq.pop();
-           
-            if(lastIndex>it.second)continue;
-            lastIndex=index;
-           
-            if(index+it.first>=nums.size()-1)
-            {
-                break;
-            }
-            for(int i=index+1;i<=(index+it.first);i++)
-            {
-               
-                 if(vis.find(i)!=vis.end())continue;
-                 vis.insert(i);
-                  pq.push({nums[i],i});
-            }
+            if(maxReach<i)return false;
+            maxReach=max(i+nums[i],maxReach);
         }
         return true;
     }
