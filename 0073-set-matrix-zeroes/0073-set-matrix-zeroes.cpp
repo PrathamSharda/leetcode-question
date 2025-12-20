e7cdc38f23070463ad7b6c139afc8f0ad4edb9a3
PRATHAM SHARDA <113026312+PrathamSharda@users.noreply.github.com>
2025-12-20 23:52:26 +0530

    0073-SET-MATRIX-ZEROES Stats: Time: 0 ms (100%), Space: 18.7 MB (50.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        queue<pair<int,int>>q;
+        int row=0;
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
             {
+                
                 if(matrix[i][j]==0)
                 {
-                    q.push({i,j});
-                }
+                   
+                   
+                    if(j==0)
+                    {
+                        row=1;
+                    }else{
+                         matrix[i][0]=0;
+                         matrix[0][j]=0;
+                    }
+                }  
             }
         }
-        
-        while(!q.empty())
-        {
-            auto [x,y]=q.front();
-            q.pop();
-            for(int i=0;i<matrix.size();i++)
+        for (int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++)
             {
-                matrix[i][y]=0;
+                if(matrix[i][0]==0||matrix[0][j]==0)matrix[i][j]=0;
             }
+        }
+        if(matrix[0][0]==0)
+        { 
             for(int i=0;i<matrix[0].size();i++)
             {
-                matrix[x][i]=0;
+                matrix[0][i]=0;
             }
         }
-
+        if(row)
+        {
+             for(int i=0;i<matrix.size();i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
     }
 };
