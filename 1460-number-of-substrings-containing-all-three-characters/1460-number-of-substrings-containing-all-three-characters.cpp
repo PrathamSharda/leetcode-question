efc9c2d29b4fbd4f46e466c0df12f68dcff8d853
PRATHAM SHARDA <113026312+PrathamSharda@users.noreply.github.com>
2026-01-11 22:31:43 +0530

    1460-NUMBER-OF-SUBSTRINGS-CONTAINING-ALL-THREE-CHARACTERS Stats: Time: 15 ms (33.51%), Space: 13.8 MB (16.08%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int>count(3,-1);
+       // vector<int>count(3,-1);
         int total=0;
+        queue<int>a;
+        queue<int>b;
+        queue<int>c;
         for(int i=0;i<s.length();i++)
         {
-            count[s[i]-'a']=i;
-            int mini=INT_MAX;
-            for(int j=0;j<3;j++)
-            {
-                mini=min(mini,count[j]);
-            }
-            if(mini==-1)continue;
-            total+=(mini)+1;
+            if(s[i]=='a')a.push(i);
+            else if(s[i]=='b')b.push(i);
+            else c.push(i);
         }
+        for(int i=0;i<s.length();i++)
+        {
+              if(a.empty()||b.empty()||c.empty())break;
+            int indA=a.front();
+            int indB=b.front();
+            int indC=c.front();
+            if(i==indA)
+            a.pop();
+            if(i==indB)
+            b.pop();
+            if(i==indC)
+            c.pop();
+
+             int maxNum=max(max(max(indA,indB),max(indC,indB)),max(max(indC,indB),max(indA,indC)));
+             total+=(s.length()-maxNum);
+            
+
+          
+        }
+       
         return total;
     }
 };
